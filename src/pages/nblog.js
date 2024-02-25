d41d007ba0d72c8792a787551137e2eec66b3a3e
Oliver Leder <oleder@slashwhy.de>
2024-02-25 22:52:56 +0100

    Finished Tutorial Part 5

--- src/pages/nblog.js
+++ src/pages/nblog.js
@@ -7,20 +7,29 @@ const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle='My Blog Posts'>
             {/* <p>My cool Posts wiasdfasdll go here</p> */}
-            <ul>
-                {data.allFile.nodes.map(node => (
-                    <li key={node.name}>{node.name}</li>
-                ))}
-            </ul>
+
+            {data.allMdx.nodes.map(node => (
+                <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                    <p>{node.excerpt}</p>
+                </article>
+            ))}
+
         </Layout>
     )
 }
 
 export const query = graphql`
-query blogs {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+query MyQuery {
+    allMdx(sort: {frontmatter: {date: DESC}}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        excerpt
       }
     }
   }`
